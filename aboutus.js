47730524d41582ecb4d0bf7bed9cdccea4b25b83
kaymid <dquanht0@gmail.com>
2022-09-18 17:41:19 +0700

    Tabs

---
+++ aboutus.js
@@ -0,0 +1,23 @@
+// const { Button } = require("bootstrap");
+
+
+// const onclick = document.querySelector('onclick');
+// const dataInput = document.querySelector('dataInput');
+// const dataShow = document.querySelector('dataShow');
+
+// Button.addEventListener('click',(e) =>{
+//     const dataDisplay = dataInput.value;
+//     const li =document.createElement('li');
+//     list.textContent = dataDisplay;
+//     dataShow.appendChild(list);
+//     dataInput.value='';
+//     dataInput.focus();
+
+// })
+
+function myFunction(){
+    //get the data display
+    let danny = document.getElementById('danny');
+
+    danny.innerHTML = 'display:block';
+}
